2e462453fa39cf5d36460fa210cd963a564c9d64
Cristian <cristianvasquez@gmail.com>
2025-06-30 17:31:15 +0200

    simplify demo

--- playground/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
-app.mount('#app')
